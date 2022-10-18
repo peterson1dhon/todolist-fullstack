59ce9c4ec9b28c3c92fd83dd3d61787ec26bcf04
peterson silva caetano <peterson_dhon@hotmail.com>
2022-10-18 11:45:44 -0300

    taskCont

---
+++ backend/src/controllers/tasksController.js
@@ -0,0 +1,7 @@
+const getAll = () => {
+
+};
+
+module.export = {
+   getAll
+};
--- backend/src/router.js
+++ backend/src/router.js
@@ -1,7 +1,8 @@
 const express =  require('express');
+const tasksController = require('./controllers/tasksController');
 
 const router = express.Router();
 
-router.get('/tasks');
+router.get('/tasks', tasksController.getAll);
 
 module.exports = router;
